9c79c195b185e58c34a9fc506cc79bc1953ce136
Connor Leech <connorleech@gmail.com>
2015-05-27 12:00:55 -0700

    update todo

--- jsframework/static/js/main.js
+++ jsframework/static/js/main.js
@@ -21,13 +21,17 @@ app.config(function($stateProvider, $urlRouterProvider){
 	$urlRouterProvider.otherwise('/');
 });
 
-app.controller('MainCtrl', function($scope, Todos){
+app.controller('MainCtrl', function($scope, Todos, $state){
 	$scope.newTodo = {};
 	$scope.addTodo = function() {
 		Todos.addOne($scope.newTodo)
 			.then(function(res){
 				console.log('response from addTodo = ', res);
 			});
+	};
+
+	$scope.toggleCompleted = function(todo) {
+		Todos.update(todo);
 	};
 
 	Todos.all().then(function(res){
@@ -42,23 +46,19 @@ app.service('Todos', function($http, BASE_URL){
 		return $http.get(BASE_URL);
 	};
 
+	Todos.update = function(updatedTodo){
+		return $http.put(BASE_URL + updatedTodo.id, updatedTodo);
+	};
+
 	Todos.addOne = function(newTodo){
 		return $http.post(BASE_URL, newTodo)
 			.success(function(data, status, headers){
 				console.log('success data =');
 				console.log(data);
-				console.log('success status =');
-				console.log(status);
-				console.log('success headers =');
-				console.log(headers);
 			})
 			.error(function(data, status, headers){
 				console.log('error data =');
 				console.log(data);
-				console.log('error status =');
-				console.log(status);
-				console.log('error headers =');
-				console.dir(headers);
 			});
 	};
 
